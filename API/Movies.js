9732f10dfdad197abc10d793fc8a0b797d4059c4
Conner Donohoo <donohoo13@gmail.com>
2018-11-20 16:28:55 -0700

    API add movie works

--- API/Movies.js
+++ API/Movies.js
@@ -1,23 +1,42 @@
-const { send } = require ('micro');
+const { send,json } = require ('micro');
 const { get, post, put, del } = require('microrouter');
 const db = require ('./DataConfig');
 
 const Movies = db.get('Movies')
 
-const getMovies = async (req, res) => {
+getMovies = async (req, res) => {
     const result = await Movies.find({}).then( results => ( results ) )
     send( res, 200, result);
 }
 
+//GET only movies you've seen a number of times.  don't know how I'd use this but it's cool.
 getMoviesBySeen = async (req, res) => {
     let result = await Movies.find({}).then( results => ( results ) )
-    console.log("search: ", req.params.seen)
+    // console.log("search: ", req.params.seen)
     result = await result.filter(movies => movies.seen >= req.params.seen)
     send(res, 200, result);
 }
 
+//GET Movies by name
+getMoviesByName = async (req, res) => {
+    let result = await Movies.find({}).then( results => ( results ) )
+    // console.log("PreFilter: ", result)
+    result = await result.filter(movie => movie.title.toLowerCase().includes(req.params.title.toLowerCase()))
+    // console.log("PostFilter: ", result)
+    send(res, 200, result);
+}
+
+//POST new movie to DB {"title": "", "releaseDate": "", "genre": [], "actors": [], "seen": Number, "image": ""}
+addMovie = async (req, res) => {
+    const body = await json(req)
+    let result = await Movies.insert(body).then( results => results )
+    send(res, 200, result)
+}
+
 module.exports = [
     get('/movies', getMovies),
-    get('/movies/seen/:seen', getMoviesBySeen)
+    get('/movies/seen/:seen', getMoviesBySeen),
+    get('/movies/title/:title', getMoviesByName),
+    post('/movies', addMovie)
 
 ]
--- UI/src/Components/Movies.js
+++ UI/src/Components/Movies.js
@@ -7,11 +7,22 @@ class Movies extends Component {
     super()
         this.state = {
             movies: [],
+            search: ''
     }
 }
 
 getMovies = async () => {
     await fetch('http://localhost:4000/movies')
+    .then(res => res.json())
+    .then(data => {this.setState({movies: data})})
+}
+
+Search = async (event) => {
+    event.preventDefault()
+    if(this.state.search === ''){
+        return this.getMovies()
+    } else
+    await fetch('http://localhost:4000/movies/title/' + this.state.search)
     .then(res => res.json())
     .then(data => {this.setState({movies: data})})
 }
@@ -24,6 +35,10 @@ componentDidMount = async () => {
         return(
             <div>
                 <h1>Movies Page works!</h1>
+                <form onSubmit={this.Search}>
+                    <input type="text" onChange={event => this.setState({search: event.target.value})} />
+                    <input type="submit" value="Search" />
+                </form>
                 <MoviesIndex data={this.state.movies} />
             </div>
         );
